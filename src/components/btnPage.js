b1911f1690cddf63960866d8307d7f210ebcb148
Sergei Osokin <sergei.osokin.rf@mail.ru>
2021-05-31 23:10:01 +0300

    pagination

---
+++ src/components/btnPage.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// export const BtnPage = (arrNumberPages) => {
+//     return (
+//         <div className="pagesList">
+//             {arrNumberPages.map((item) => {
+//                 return <i className="pagesList" key={item} onClick={() => { openMoreCity(item) }}>{item}</i>
+//             })}
+//         </div>
+//     )
+// }
--- src/pages/WatchCities.js
+++ src/pages/WatchCities.js
@@ -1,26 +1,52 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
+import { BtnPage } from '../components/btnPage';
 import { ListCities } from '../components/listCities';
 import { useHttp } from '../hooks/http.hook';
 
 export const WatchCities = () => {
     const [cities, setCities] = useState([]);
+    const [pageQwe, setPage] = useState(1);
+    const [countPages, setCountPages] = useState(0);
+    const [pagesArr, setpagesArr] = useState([]);
+    const [isDisabled, setDisabled] = useState(false);
+
     const { request } = useHttp();
     const history = useHistory();
+    const limit = 5;
 
-    const getCities = useCallback( async () => {
-        const fetched = await request('http://localhost:3001/cities/?_page=1&_limit=2');
-        console.log(fetched)
+    const getCities = useCallback(async (list) => {
+        const fetched = await request(`http://localhost:3001/cities/?_page=${list}&_limit=5`);
         setCities(fetched);
     }, [request])
-
-    function openMoreCity() {
-        console.log(cities)
+    //по кнопке страницы
+    function getCityByPage(page) {
+        getCities(page);
+        if(page === 1) {
+            setPage(1);
+            setDisabled(false);
+        }
     }
-
+    // по кнопке загрузить города
+    function getCityByBtn() {
+        if(pageQwe < countPages) {
+            console.log(pageQwe)
+            getCities(pageQwe + 1);
+            setPage(pageQwe + 1);
+            return;
+        } else {
+            setDisabled(true)
+            return;
+        }
+    }
+    // при загрузке
     useEffect(() => {
-        getCities();
-    }, [getCities])
+        getCities(pageQwe);
+        request(`http://localhost:3001/cities`)
+            .then((res) => {
+                setCountPages(Math.ceil(res.length / limit));
+            })
+    }, [getCities, request])
 
     function newCity() {
         history.push('/createcity');
@@ -29,8 +55,17 @@ export const WatchCities = () => {
     return (
         <div className="watchCities">
             <input className="button buttonAdd" type="button" value={"ДОБАВИТЬ ГОРОД"} onClick={() => newCity()} />
-            <ListCities cities={cities} />
-            <input className="button buttonLoad" type="button" value="ЗАГРУЗИТЬ ГОРОДА" onClick={() => openMoreCity()} />
+            < ListCities cities={cities} />
+            <div className="pagesList">
+                {[1, 2, 3].map((item) => {
+                    return <i 
+                    className="pagesList__pageNumber" 
+                    key={item} 
+                    onClick={() => { getCityByPage(item) }}
+                    >{item}</i>
+                })}
+            </div>
+            < input className="button buttonLoad" type="button" value="ЗАГРУЗИТЬ ГОРОДА" disabled={isDisabled} onClick={() => getCityByBtn()} />
         </div>
     )
 }
